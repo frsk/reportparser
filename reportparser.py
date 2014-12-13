4bc41d1b1dbcfcc13fe9d08006a98f63c652196b
Fredrik Skolmli <fredrik@skolmli.no>
2014-12-13 23:22:21 +0100

    Rename windows_file to filename

--- reportparser.py
+++ reportparser.py
@@ -39,7 +39,7 @@ hashmatch = re.compile(r"\b([a-fA-F0-9]{128}|[a-fA-F0-9]{32}|[a-fA-F0-9]{40})\b"
 cvematch = re.compile(r"CVE-\d{4}-\d+\b", re.I)
 domainmatch = re.compile(r"\b(([a-zA-Z.-]{4,})\.([a-z]{1,8}))\b", re.I)
 urlmatch = re.compile(r"""((?:https?|ftp)://[^\s/$.?#].[^\s]*)""")
-winfilematch = re.compile(r"\b(([a-zA-Z]{2,255})\.(exe|dll|msi))\b", re.I)
+filematch = re.compile(r"\b(([a-zA-Z]{2,255})\.(exe|dll|msi))\b", re.I)
 
 try:
     config = ConfigParser.ConfigParser()
@@ -96,7 +96,7 @@ def process(filename):
     result['content']['ipv4'] = []
     result['content']['cve'] = []
     result['content']['url'] = []
-    result['content']['windows_file'] = []
+    result['content']['filename'] = []
 
     for x in domainmatch.findall(output.getvalue()):
         if x[0].lower() in result['content']['domain']:
@@ -118,10 +118,10 @@ def process(filename):
             continue
         result['content']['cve'].append(x)
 
-    for x in winfilematch.findall(output.getvalue()):
-        if x[0].lower() in result['content']['windows_file']:
+    for x in filematch.findall(output.getvalue()):
+        if x[0].lower() in result['content']['filename']:
             continue
-        result['content']['windows_file'].append(x[0].lower())
+        result['content']['filename'].append(x[0].lower())
 
     for x in urlmatch.findall(output.getvalue()):
         if x in result['content']['url']:
