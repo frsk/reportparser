a9c5bf8c8f834577a306e7f6ab7e04facbe503bc
Fredrik Skolmli <fredrik@skolmli.no>
2014-12-13 22:35:14 +0100

    Add domain support

--- reportparser.py
+++ reportparser.py
@@ -21,6 +21,7 @@ parser.add_argument('-c', '--config',
                     help="Configuration file",
                     default="~/.reportparser.conf",
                     type=str)
+
 parser.add_argument('-d', '--disable-mongo',
                     help="Do not store in MongoDB",
                     action="store_true")
@@ -36,6 +37,8 @@ args = parser.parse_args()
 find_ip4 = re.compile(r"\b((?:\d{1,3}\.){3}\d{1,3})")
 hashmatch = re.compile(r"\b([a-fA-F0-9]{128}|[a-fA-F0-9]{32}|[a-fA-F0-9]{40})\b")
 cvematch = re.compile(r"CVE-\d{4}-\d+\b", re.I)
+domainmatch = re.compile(r"\b(([a-zA-Z.-]{4,})\.([a-z]{1,8}))\b", re.I)
+
 try:
     config = ConfigParser.ConfigParser()
     config.readfp(open(os.path.expanduser(args.config)))
@@ -86,9 +89,15 @@ def process(filename):
                 password=password, caching=caching, check_extractable=True)
 
     result['content'] = {}
+    result['content']['domain'] = []
     result['content']['hash'] = []
     result['content']['ipv4'] = []
     result['content']['cve'] = []
+
+    for x in domainmatch.findall(output.getvalue()):
+        if x[0].lower() in result['content']['domain']:
+            continue
+        result['content']['domain'].append(x[0].lower())
 
     for x in hashmatch.findall(output.getvalue()):
         if x in result['content']['hash']:
@@ -104,6 +113,7 @@ def process(filename):
         if x in result['content']['cve']:
             continue
         result['content']['cve'].append(x)
+
 
     result['file'] = {}
     result['file']['parsed'] = time.time()
